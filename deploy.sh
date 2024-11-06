5a29a5f1a9d7df8b9689f8f36205af6a8e5da2a0
Abdallah Geha <geha.abdallah@gmail.com>
2024-11-06 17:14:24 +0100

    consume db with prisma

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,9 @@ IMAGE_NAME="lira:latest"
 # Stop and remove the old container if it exists
 if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
     echo "Stopping and removing old container..."
-    docker stop $CONTAINER_NAME
-    docker rm $CONTAINER_NAME
+    docker compose down
 fi
 
-# Remove the old image if needed
-if [ "$(docker images -q $IMAGE_NAME)" ]; then
-    echo "Removing old image..."
-    docker rmi $IMAGE_NAME
-fi
-
-# Build a new image
-echo "Building new image..."
-docker build -t $IMAGE_NAME .
-
-# Run a new container from the image
-echo "Running new container..."
-docker run -d --name $CONTAINER_NAME -p 3000:3000 $IMAGE_NAME
+# Build and run new containers using Docker Compose
+echo "Building and running containers using Docker Compose..."
+docker compose up --build -d
--- server/src/controllers/apiController.ts
+++ server/src/controllers/apiController.ts
@@ -1,6 +1,34 @@
 import { Router, Request, Response } from "express";
+import pool from "../db/db";
+import { PrismaClient } from "@prisma/client";
 
 export const apiRoutes = Router();
+
+const prisma = new PrismaClient();
+
+apiRoutes.get("/users", async (req: Request, res: Response) => {
+  try {
+    // const result = await pool.query("SELECT * FROM users");
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+apiRoutes.get("/adduser", async (req: Request, res: Response) => {
+  try {
+    const newUsers = await prisma.user.create({
+      data: {
+        name: crypto.randomUUID(),
+        email: `${crypto.randomUUID()}@emil.com`,
+      },
+    });
+    res.status(201).json(newUsers);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 apiRoutes.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
